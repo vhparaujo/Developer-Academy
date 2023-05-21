daf1b40c22b821d3df3c4dc5b6e41f848c79aec2
vhparaujo <vhugopacheco59@gmail.com>
2023-05-20 23:47:55 -0300

    everyBlocks funcionando

--- BitcoinBlockExplorer/BitcoinBlockExplorer/Screens/EveryBlocks.swift
+++ BitcoinBlockExplorer/BitcoinBlockExplorer/Screens/EveryBlocks.swift
@@ -1,85 +1,95 @@
-////
-////  EveryBlocks.swift
-////  TesteAPI
-////
-////  Created by Victor Hugo Pacheco Araujo on 16/05/23.
-////
 //
-//import SwiftUI
+//  EveryBlocks.swift
+//  TesteAPI
 //
-//struct EveryBlocks: View {
-//  @State var heightBlock: Int = 0
-//  @State var hashBlock: String = ""
-//  @StateObject var blockData = BlockDataEveryBlocks()
-//  @State var abrirModal: Bool = false
-//  @State var searchText = ""
-//  let colunas = [GridItem(spacing: 5), GridItem(spacing: 5)]
-//  
-//  var body: some View {
-//    NavigationStack{
-//      VStack{
-//        ScrollView{
-//          
-//          HStack {
-//            Text("Bloco").foregroundColor(Color("cinza")).font(.system(size: 17))
-//            Spacer()
-//          }.padding()
-//          LazyVGrid(columns: colunas, spacing: 20) {
-//            ForEach(blockData.blockDatas, id: \.self) { blocks in
-//              
-//              Button{
-//                abrirModal.toggle()
-//              } label: {
-//                ZStack{
-//                  RoundedRectangle(cornerRadius: 7).foregroundColor(Color("caixas")).frame(width: 160,height: 109)
-//                  VStack{
-//                    let tamanho = String(format: "%.2f", (blocks.size / 1000000))
-//                    Text("\(blocks.height)").foregroundColor(Color("laranja")).font(.system(size: 15))
-//                    Text("~\(Int(blocks.extras.medianFee)) sat/vB").foregroundColor(Color("cinza")).font(.system(size: 12))
-//                    Text("\(tamanho) MB").foregroundColor(Color("cinza")).font(.system(size: 12))
-//                    Text("\(blocks.tx_count) transações").foregroundColor(Color("cinza")).font(.system(size: 12))
-//                    Text("\(blocks.formatTimestamp(blocks.timestamp))").foregroundColor(Color("cinza")).font(.system(size: 12))
-//                  }
-//                }.onTapGesture {
-//                  hashBlock = blocks.id
-//                  heightBlock = blocks.height
-//                  print("\(blocks.id)")
-//                  abrirModal.toggle()
-//                }
-//              }
-//            }
-//          }
-//          
-//        }
-//      }
-//      .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Blocos, endereços ou transações")
-//      .padding()
-//      
-//      .sheet(isPresented: $abrirModal) {
-//        EachBlock(abrirModal: $abrirModal, hashBlock: $hashBlock, heightBlock: $heightBlock).presentationDetents([.height(650), .fraction(0.90)])
-//          .presentationBackground(Color("azul"))
-//      }
-//      
-//      .onAppear {
-//        blockData.fetch()
-//      }
-//      
-//      .navigationBarTitleDisplayMode(.inline)
-//      .toolbar {
-//        ToolbarItem(placement: .navigationBarLeading) {
-//          Image("bitcoin").resizable().frame(width: 30, height: 30)
-//        }
-//        ToolbarItem(placement: .principal) {
-//          Text("Bitcoin Blockchain Explorer").foregroundColor(Color("laranja")).font(.system(size: 20))
-//        }
-//      }
-//      .background(Color("azul"))
-//    }
-//  }
-//}
+//  Created by Victor Hugo Pacheco Araujo on 16/05/23.
 //
-//struct EveryBlocks_Previews: PreviewProvider {
-//  static var previews: some View {
-//    EveryBlocks()
-//  }
-//}
+
+import SwiftUI
+
+struct EveryBlocks: View {
+  
+  @State var timestamp: String = ""
+  @State var numberTransactions: Int64 = 0
+  @State var blockMiner: String = ""
+  @State var medianFee: Double = 0
+  @State var blockSize: Double = 0
+  @State var heightBlock: Int = 0
+  @State var hashBlock: String = ""
+  @StateObject var blockData = BlockDataEveryBlocks()
+  @State var abrirModal: Bool = false
+  @State var searchText = ""
+  let colunas = [GridItem(spacing: 5), GridItem(spacing: 5)]
+  
+  var body: some View {
+    NavigationStack{
+      VStack{
+        ScrollView{
+          
+          HStack {
+            Text("Bloco").foregroundColor(Color("cinza")).font(.system(size: 17))
+            Spacer()
+          }.padding()
+          LazyVGrid(columns: colunas, spacing: 20) {
+            ForEach(blockData.blockDatas, id: \.self) { blocks in
+              
+              Button{
+                abrirModal.toggle()
+              } label: {
+                ZStack{
+                  RoundedRectangle(cornerRadius: 7).foregroundColor(Color("caixas")).frame(width: 160,height: 109)
+                  VStack{
+                    let tamanho = String(format: "%.2f", (blocks.size / 1000000))
+                    Text("\(blocks.height)").foregroundColor(Color("laranja")).font(.system(size: 15))
+                    Text("~\(Int(blocks.extras.medianFee)) sat/vB").foregroundColor(Color("cinza")).font(.system(size: 12))
+                    Text("\(tamanho) MB").foregroundColor(Color("cinza")).font(.system(size: 12))
+                    Text("\(blocks.tx_count) transações").foregroundColor(Color("cinza")).font(.system(size: 12))
+                    Text("\(blocks.formatTimestamp(blocks.timestamp))").foregroundColor(Color("cinza")).font(.system(size: 12))
+                  }
+                }.onTapGesture {
+                  hashBlock = blocks.id
+                  heightBlock = blocks.height
+                  medianFee = blocks.extras.medianFee
+                  blockSize = blocks.size
+                  blockMiner = blocks.extras.pool.name
+                  numberTransactions = blocks.tx_count
+                  timestamp = blocks.formatTimestampWithHour(blocks.timestamp)
+                  abrirModal.toggle()
+                }
+              }
+            }
+          }
+          
+        }
+      }
+      .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Blocos, endereços ou transações")
+      .padding()
+      
+      .sheet(isPresented: $abrirModal) {
+        EachBlock(timestamp: $timestamp,numberTransactions: $numberTransactions, blockMiner: $blockMiner, medianFee: $medianFee, blockSize: $blockSize, hashBlock: $hashBlock, heightBlock: $heightBlock, abrirModal: $abrirModal).presentationDetents([.height(650), .fraction(0.90)])
+          .presentationBackground(Color("azul"))
+      }
+      
+      .onAppear {
+        blockData.fetch()
+      }
+      
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarLeading) {
+          Image("bitcoin").resizable().frame(width: 30, height: 30)
+        }
+        ToolbarItem(placement: .principal) {
+          Text("Bitcoin Blockchain Explorer").foregroundColor(Color("laranja")).font(.system(size: 20))
+        }
+      }
+      .background(Color("azul"))
+    }
+  }
+}
+
+struct EveryBlocks_Previews: PreviewProvider {
+  static var previews: some View {
+    EveryBlocks()
+  }
+}
